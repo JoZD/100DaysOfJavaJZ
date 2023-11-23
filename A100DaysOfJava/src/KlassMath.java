bf390e560526f22d3be89fb5a049a7f3f6cf9d79
Draster <jorlan.jesus@gmail.com>
2023-11-22 19:33:25 -0600

    Day 5  Completed!

---
+++ A100DaysOfJava/src/KlassMath.java
@@ -0,0 +1,14 @@
+
+public class KlassMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		double root = Math.sqrt(4);
+		
+		System.out.println(root);
+		
+	}
+
+}
