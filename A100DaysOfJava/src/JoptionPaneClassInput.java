e80767340d13ae24a3759f532179761c9b3064da
Draster <jorlan.jesus@gmail.com>
2023-11-27 20:54:48 -0600

    Day 7 Completed

---
+++ A100DaysOfJava/src/JoptionPaneClassInput.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class JoptionPaneClassInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = JOptionPane.showInputDialog("Please enter your name: ");
+		
+		//Display the input 
+		JOptionPane.showMessageDialog(null,name);
+
+	}
+
+}
